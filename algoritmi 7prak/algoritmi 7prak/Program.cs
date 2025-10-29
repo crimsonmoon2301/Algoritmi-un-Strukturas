28f739d919a91074b141d45610693890a63446cf
Artis P. <150836006+crimsonmoon2301@users.noreply.github.com>
2025-10-29 11:53:14 +0200

    Update Program.cs

--- algoritmi 7prak/algoritmi 7prak/Program.cs
+++ algoritmi 7prak/algoritmi 7prak/Program.cs
@@ -96,11 +96,12 @@ namespace algoritmi_7prak
         }
         static void Sagatavosana_uzpilde()
         {
+            // Drošības pēc izveido failu lai ir kur rakstīt.
             if (!File.Exists(csv))
             {
                 using (StreamWriter writer = new StreamWriter(csv))
                 {
-                    writer.WriteLine($"# CSV pirmo reizi izveidots: {DateTime.Now}");
+                    writer.WriteLine($"# CSV temp galvene");
                 }
             }
             using (StreamWriter sw = new StreamWriter(csv))
@@ -108,6 +109,8 @@ namespace algoritmi_7prak
                 sw.WriteLine("Izmers,tips,Algoritms,Operacijas");
                 // Pārsūta uz masīvu izveidi
                
+                // Aizpilda masīvus ar elementu skaitu no 100 līdz 10000, ar 100 soli.
+                // Katrs jaunais elements ir par 100 vairāk
                 for (int n = 100; n <= 10000; n += 100)
                 {
                     // Nodefinē masīvu veidus
@@ -117,21 +120,25 @@ namespace algoritmi_7prak
                     {
                         int[] mass = MasivuIzveide(n, veids);
 
+                        // Izveido kopijas ar kuriem darboties. Gadījās ka sajauc rezultātus ja viss ir no viena.
                         int[] masscopy = (int[])mass.Clone();
                         operacijasBubble = 0;
                         bubbleSort(masscopy, n);
                         sw.WriteLine($"{n},{veids},BubbleSort,{operacijasBubble}");
+                        //printarray(masscopy);
 
                         int[] masscopy1 = (int[])mass.Clone();
                         operacijasInsert = 0;
                         insertionSort(masscopy1);
                         sw.WriteLine($"{n},{veids},InsertionSort,{operacijasInsert}");
+                        //printarray(masscopy1);
 
                         int[] masscopy2 = (int[])mass.Clone();
                         operacijasShell = 0;
                         ShellSort(masscopy2);
                         sw.WriteLine($"{n},{veids},ShellSort,{operacijasShell}");
-                        sw.WriteLine("==========");
+                        //printarray(masscopy2);
+                        //sw.WriteLine("==========");
 
                     }
                     
@@ -174,9 +181,8 @@ namespace algoritmi_7prak
                 case "Dilst":
                     for (int i = 0; i < n; i++)
                     {
-                        arr[i] = i;
+                        arr[i] = n-1-i;
                     }
-                    Array.Reverse(arr);
                     break;
                 case "Gadijuma":
                     for (int i = 0; i < n; i++)
@@ -184,21 +190,40 @@ namespace algoritmi_7prak
                         arr[i] = rand.Next(n);
                     }
                     break;
-                case "Unique": // es hvz ko te likt. Met ārpus robežām.
+                case "Unique": // īslaīcīgs risinājums, tiklīdz atrodu īsto
+                    //for (int i = 0; i < n; i++)
+                    //{
+                    //    arr[i] = rand.Next(n);
+                    //    bool isDuplicate = false;
+                    //    for (int j = 0; j < i; j++)
+                    //    {
+                    //        if (arr[i] == arr[j])
+                    //        {
+                    //            isDuplicate = true;
+                    //        }
+                    //    }
+                    //    if (!isDuplicate)
+                    //    {
+                    //        arr[i] = i;
+                    //    }
+                    //}
                     for (int i = 0; i < n; i++)
                     {
-                        arr[i] = rand.Next(n);
-                        bool isDuplicate = false;
-                        for (int j = 0; j < i; j++)
+                        bool unique = false;
+                        // Ja gadienā trāpās vienādi gadījuma skaitļi, aptur ģenerēšanu
+                        while (!unique)
                         {
-                            if (arr[i] == arr[j])
+                            arr[i] = rand.Next(n);
+                            unique = true;
+
+                            for (int j = 0; j < i; j++)
                             {
-                                isDuplicate = true;
+                                if (arr[i] == arr[j])
+                                {
+                                    unique = false;
+                                    break;
+                                }
                             }
-                        }
-                        if (!isDuplicate)
-                        {
-                            arr[i] = i;
                         }
                     }
                     //List<int> navunique = new List<int>(arr);
@@ -210,8 +235,45 @@ namespace algoritmi_7prak
                     //arr = navunique.Distinct().ToArray();
                     break;
                 case "GandrizAug":
+                    for (int i = 0; i < n; i++)
+                    {
+                        arr[i] = i;
+                    }
+                    int sajauc = (int)(n * 0.3);
+
+                    for (int i = 0; i < sajauc; i++)
+                    {
+                        int a = rand.Next(n);
+                        int b = rand.Next(n);
+                        if (a!=b)
+                        {
+                            int temp = arr[a];
+                            arr[a] = arr[b];
+                            arr[b] = temp;
+                        }
+                    }
                     break;
                 case "GandrizDilst":
+                    // Sāc no otrā gala
+                    for (int i = 0; i < n; i++)
+                    {
+                        arr[i] = n - 1 - i;
+                    }
+                    // Paņem tagadējo elementu skaitu un sareizina ar 30% (0,3)
+                    int sajaucdilst = (int)(n * 0.3);
+                    // Izmanto mainīgo lai varētu sajaukt elementus
+                    for (int i = 0; i < sajaucdilst; i++)
+                    {
+                        int a = rand.Next(n);
+                        int b = rand.Next(n);
+                        // Ja gadījuma skaitlis nav  vienāds ar viens otru, sajauc vietām
+                        if (a != b)
+                        {
+                            int temp = arr[a];
+                            arr[a] = arr[b];
+                            arr[b] = temp;
+                        }
+                    }
                     break;
                 default:
                     Console.WriteLine("Kautkas aizgāja greizi");
@@ -219,41 +281,66 @@ namespace algoritmi_7prak
             }
             return arr;
         }
-        static int ShellSort(int[] arr)
+
+        static void ShellSort(int[] arr)
         {
-            int n = arr.Length;
-
-            // Start with a big gap, 
-            // then reduce the gap
-            for (int gap = n / 2; gap > 0; gap /= 2)
+            int i, j, H;
+            int tmpa;
+            // Paņem masīvu un sadala to divās daļās, pakāpeniski samazina tiklīdz sasniedz intervālu 1
+            for (H = arr.Count()/2; H >=1 ; H=H/2)
             {
-                // Do a gapped insertion sort for this gap size.
-                // The first gap elements a[0..gap-1] are already
-                // in gapped order keep adding one more element
-                // until the entire array is gap sorted
-                for (int i = gap; i < n; i += 1)
+                // Sadala segmentos un tos sakārto
+                for (i = H; i < arr.Count(); i=i+H)
                 {
-                    // add a[i] to the elements that have
-                    // been gap sorted save a[i] in temp and
-                    // make a hole at position i
-                    int temp = arr[i];
-
-                    // shift earlier gap-sorted elements up until
-                    // the correct location for a[i] is found
-                    int j;
-                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    // Ieliek tagadējo elementu īslaicīgā mainīgā priekš apmainīšanas
+                    tmpa = arr[i];
+                    for (j = i-H; (j >= 0) && (arr[j]>tmpa); j=j-H)
                     {
-                        arr[j] = arr[j - gap];
+                        // Ja ir lielāks par temp, apmaina vietām
+                        arr[j + H] = arr[j];
                         operacijasShell++;
                     }
-                    // put temp (the original a[i]) 
-                    // in its correct location
-                    arr[j] = temp;
+                    // Ievieto īslaicīgo mainīgo pareizā pozīcijā
+                    arr[j + H] = tmpa;
+                    
                 }
-                
             }
-            return 0;
         }
+        //static int ShellSort(int[] arr)
+        //{
+        //    int n = arr.Length;
+
+        //    // Start with a big gap, 
+        //    // then reduce the gap
+        //    for (int gap = n / 2; gap > 0; gap /= 2)
+        //    {
+        //        // Do a gapped insertion sort for this gap size.
+        //        // The first gap elements a[0..gap-1] are already
+        //        // in gapped order keep adding one more element
+        //        // until the entire array is gap sorted
+        //        for (int i = gap; i < n; i += 1)
+        //        {
+        //            // add a[i] to the elements that have
+        //            // been gap sorted save a[i] in temp and
+        //            // make a hole at position i
+        //            int temp = arr[i];
+
+        //            // shift earlier gap-sorted elements up until
+        //            // the correct location for a[i] is found
+        //            int j;
+        //            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+        //            {
+        //                arr[j] = arr[j - gap];
+        //                operacijasShell++;
+        //            }
+        //            // put temp (the original a[i]) 
+        //            // in its correct location
+        //            arr[j] = temp;
+        //        }
+
+        //    }
+        //    return 0;
+        //}
         static void bubbleSort(int[] arr, int n)
         {
             int i, j, temp;
@@ -263,55 +350,58 @@ namespace algoritmi_7prak
                 swapped = false;
                 for (j = 0; j < n - i - 1; j++)
                 {
+                    // Salīdzina vai tagadējais elements ir lielāks par nākamo
                     if (arr[j] > arr[j + 1])
                     {
 
-                        // Swap arr[j] and arr[j+1]
+                        // Ja ir, apmaina vietām
                         temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
                         swapped = true;
+                        operacijasBubble++;
                     }
                 }
-                // If no two elements were
-                // swapped by inner loop, then break
+                // Ja elementi vairs netiek apmainīti, aptur darbību
                 if (swapped == false)
                     break;
-                operacijasBubble++;
             }
         }
 
         
         static void insertionSort(int[] arr)
         {
+            // Iegūst masīva izmēru/garumu
             int n = arr.Length;
             for (int i = 1; i < n; ++i)
             {
+                // Norāda "atslēgu" kā tagadējo elementu no masīva
                 int key = arr[i];
+                // Iepriekšējais elements pirms tagadējā
                 int j = i - 1;
 
-                /* Move elements of arr[0..i-1], that are
-                   greater than key, to one position ahead
-                   of their current position */
+                // Pārvieto elementus kuri ir lielāki par tagadējo vienu pozīciju uz priekšu  
                 while (j >= 0 && arr[j] > key)
                 {
                     arr[j + 1] = arr[j];
                     j = j - 1;
-                    operacijasInsert++;
+                    operacijasInsert++; // Skaita operāciju skaitu priekš ieraksta failā
                 }
-                operacijasInsert++;
                 arr[j + 1] = key;
-                
+                //if (j+1 != i)
+                //{
+                //    operacijasInsert++;
+                //}
             }
         }
-        //static void printArrayInsert(int[] arr)
-        //{
-        //    int n = arr.Length;
-        //    for (int i = 0; i < n; ++i)
-        //        Console.Write(arr[i] + " ");
-
-        //    Console.WriteLine();
-        //}
+        static void printarray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+        }
+        ////    Console.WriteLine();
+        ////}
         //static void printArray(int[] arr, int size)
         //{
         //    int i;
